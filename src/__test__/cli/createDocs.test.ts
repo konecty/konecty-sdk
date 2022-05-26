f487be53f747af68af2f8b4adb6b29cf08b12f8e
Carlos Gomes <caduvitorinog@gmail.com>
2022-05-26 09:30:07 -0300

    test: refactor test to accept br tag with closing tag

--- src/__test__/cli/createDocs.test.ts
+++ src/__test__/cli/createDocs.test.ts
@@ -120,13 +120,13 @@ describe('Konecty cli create doc command', () => {
 | Config | Value |
 | ------ | ----- |
 | \`collection\` | users |
-| \`description\` | <pre lang=\"yaml\">en: System users<br>pt_BR: Usuários do sistema<br></pre> |
+| \`description\` | <pre lang=\"yaml\">en: System users<br/>pt_BR: Usuários do sistema<br/></pre> |
 | \`icon\` | user |
-| \`label\` | <pre lang=\"yaml\">en: User<br>pt_BR: Usuário<br></pre> |
+| \`label\` | <pre lang=\"yaml\">en: User<br/>pt_BR: Usuário<br/></pre> |
 | \`menuSorter\` | 10 |
 | \`name\` | User |
-| \`namespace\` | <pre lang=\"yaml\">- base<br></pre> |
-| \`plurals\` | <pre lang=\"yaml\">en: Users<br>pt_BR: Usuários<br></pre> |
+| \`namespace\` | <pre lang=\"yaml\">- base<br/></pre> |
+| \`plurals\` | <pre lang=\"yaml\">en: Users<br/>pt_BR: Usuários<br/></pre> |
 | \`saveHistory\` | true |
 | \`type\` | document |
 | \`parent\` | base:User |
@@ -134,12 +134,12 @@ describe('Konecty cli create doc command', () => {
 
 | key | label | type | required | list | options | other |
 | --- | ----- | ---- | -------- | ---- | ------- | ----- |
-| \`active\` | <pre lang=\"yaml\">en: Active<br>pt_BR: Ativo<br></pre> | \`boolean\` | \`true\` | \`false\` |  | \`defaultValue\`: true<br>\`isSortable\`: true<br>\`isInherited\`: true |
-| \`_createdAt\` | <pre lang=\"yaml\">en: Created At<br>pt_BR: Criado em<br></pre> | \`dateTime\` | \`false\` | \`false\` |  | \`isSortable\`: true<br>\`isInherited\`: true |
-| \`_createdBy\` | <pre lang=\"yaml\">en: Created by<br>pt_BR: Criado por<br></pre> | \`lookup\` | \`false\` | \`false\` |  | \`document\`: User<br>\`descriptionFields\`: <pre lang=\"yaml\">- name<br>- group.name<br></pre><br>\`isSortable\`: true<br>\`isInherited\`: true |
-| \`_updatedAt\` | <pre lang=\"yaml\">pt_BR: Atualizado em<br>en: Updated At<br></pre> | \`dateTime\` | \`false\` | \`false\` |  | \`isSortable\`: true<br>\`isInherited\`: true |
-| \`_updatedBy\` | <pre lang=\"yaml\">en: Updated by<br>pt_BR: Atualizado por<br></pre> | \`lookup\` | \`false\` | \`false\` |  | \`document\`: User<br>\`descriptionFields\`: <pre lang=\"yaml\">- name<br>- group.name<br></pre><br>\`isInherited\`: true |
-| \`_user\` | <pre lang=\"yaml\">en: User<br>pt_BR: Usuário<br></pre> | \`lookup\` | \`false\` | \`true\` |  | \`document\`: User<br>\`descriptionFields\`: <pre lang=\"yaml\">- name<br>- group.name<br>- active<br></pre><br>\`detailFields\`: <pre lang=\"yaml\">- phone<br>- emails<br></pre><br>\`isSortable\`: true<br>\`isInherited\`: true |
+| \`active\` | <pre lang=\"yaml\">en: Active<br/>pt_BR: Ativo<br/></pre> | \`boolean\` | \`true\` | \`false\` |  | \`defaultValue\`: true<br/>\`isSortable\`: true<br/>\`isInherited\`: true |
+| \`_createdAt\` | <pre lang=\"yaml\">en: Created At<br/>pt_BR: Criado em<br/></pre> | \`dateTime\` | \`false\` | \`false\` |  | \`isSortable\`: true<br/>\`isInherited\`: true |
+| \`_createdBy\` | <pre lang=\"yaml\">en: Created by<br/>pt_BR: Criado por<br/></pre> | \`lookup\` | \`false\` | \`false\` |  | \`document\`: User<br/>\`descriptionFields\`: <pre lang=\"yaml\">- name<br/>- group.name<br/></pre><br/>\`isSortable\`: true<br/>\`isInherited\`: true |
+| \`_updatedAt\` | <pre lang=\"yaml\">pt_BR: Atualizado em<br/>en: Updated At<br/></pre> | \`dateTime\` | \`false\` | \`false\` |  | \`isSortable\`: true<br/>\`isInherited\`: true |
+| \`_updatedBy\` | <pre lang=\"yaml\">en: Updated by<br/>pt_BR: Atualizado por<br/></pre> | \`lookup\` | \`false\` | \`false\` |  | \`document\`: User<br/>\`descriptionFields\`: <pre lang=\"yaml\">- name<br/>- group.name<br/></pre><br/>\`isInherited\`: true |
+| \`_user\` | <pre lang=\"yaml\">en: User<br/>pt_BR: Usuário<br/></pre> | \`lookup\` | \`false\` | \`true\` |  | \`document\`: User<br/>\`descriptionFields\`: <pre lang=\"yaml\">- name<br/>- group.name<br/>- active<br/></pre><br/>\`detailFields\`: <pre lang=\"yaml\">- phone<br/>- emails<br/></pre><br/>\`isSortable\`: true<br/>\`isInherited\`: true |
 `;
 		const packageJsonContent = `{ "version": "1.0.0" }`;
 		const inputFile = './metadata/User.json';
