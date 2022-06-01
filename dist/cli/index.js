a6c55f9b318bdf863e66ed1fc070962c61577716
Derotino Silveira <silveirado@gmail.com>
2022-06-01 15:53:37 -0300

    ci: remove dist from codebase

--- dist/cli/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _createProgram = _interopRequireDefault(require("./createProgram"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-const program = (0, _createProgram.default)();
-program.parse(process.argv);
